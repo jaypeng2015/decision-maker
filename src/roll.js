63b0f21a335777d5240cae5edb4d9083c9235b8a
Jingwei Peng <jay.peng@inlight.com.au>
2017-07-27 15:02:13 +1000

    Fix wrong header.

--- src/roll.js
+++ src/roll.js
@@ -15,7 +15,7 @@ module.exports.handler = (event, context, callback) => {
     statusCode: 200,
     headers: {
       // Required for CORS support to work
-      'Access-Control-Allow-Origin': '*;',
+      'Access-Control-Allow-Origin': '*',
 
       // Required for cookies, authorization headers with HTTPS
       'Access-Control-Allow-Credentials': true,
