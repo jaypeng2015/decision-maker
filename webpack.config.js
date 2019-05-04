a258c3867074d74758b467f78920b7956b36786b
Jingwei Peng <jay.peng@inlight.com.au>
2019-05-04 11:48:35 +1000

    chore(devops): optimise webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
   // output: set by the plugin
   target: 'node',
   devtool: 'source-map',
-  externals: [
-    /aws-sdk/, // Available on AWS Lambda
-  ],
+  externals: [nodeExternals()],
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   resolve: { extensions: ['.ts', '.tsx', '.js'] },
+  optimization: {
+    nodeEnv: false,
+    minimize: false,
+  },
   module: {
     rules: [
       {
